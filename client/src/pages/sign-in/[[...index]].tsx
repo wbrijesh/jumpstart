2491268cc0fef6a58df6833ed018c8ec3ba3ea9b
Brijesh Wawdhane <brijesh@wawdhane.com>
2024-01-24 04:13:24 +0530

    feat: auth client side setup with custom components

---
+++ client/src/pages/sign-in/[[...index]].tsx
@@ -0,0 +1,9 @@
+import SignInForm from "@/components/auth/SignInForm";
+
+export default function Page() {
+  return (
+    <>
+      <SignInForm />
+    </>
+  );
+}
---
+++ client/src/pages/sign-up/[[...index]].tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/components/auth/SignUpForm";
+
+export default function Page() {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+}
